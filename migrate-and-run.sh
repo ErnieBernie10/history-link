2527aba80506075660231ab1c16183e787ddcb61
ErnieBernie10 <arne.boedt1@gmail.com>
2025-03-01 18:42:49 +0100

    update again

--- migrate-and-run.sh
+++ migrate-and-run.sh
@@ -2,7 +2,7 @@
 set -e  # Exit immediately if any command fails
 
 echo "Running database migrations..."
-/usr/local/bin/dbmate up --migrations-dir="/app/db/migrations"
+/usr/local/bin/dbmate --migrations-dir "/app/db/migrations" up
 
 echo "Starting application..."
 exec /app/main  # Use exec to replace shell with your Go app
