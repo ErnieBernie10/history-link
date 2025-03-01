a0c6745fa9e8b1717fbb7b7fa1db940cbb34102d
ErnieBernie10 <arne.boedt1@gmail.com>
2025-03-01 18:29:38 +0100

    Update path in migration scrpt

--- migrate-and-run.sh
+++ migrate-and-run.sh
@@ -2,7 +2,7 @@
 set -e  # Exit immediately if any command fails
 
 echo "Running database migrations..."
-/usr/local/bin/dbmate up --migrations-dir /app/db/migrations --env-file /app/.env
+/usr/local/bin/dbmate up --migrations-dir "/app/db/migrations" --env-file "/app/.env"
 
 echo "Starting application..."
 exec /app/main  # Use exec to replace shell with your Go app
